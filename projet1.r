e0ea040f3a7da56b11f66e1c74601b2e8480fcca
David Grenier <davidgrenier@gmail.com>
2018-03-07 17:17:37 -0500

    Much changes

--- projet1.r
+++ projet1.r
@@ -1,24 +1,63 @@
 # install.packages("ggplot2",'.',dep=T,repos="http://cran.stat.sfu.ca")
 .libPaths('.')
 library(ggplot2)
+library(zeallot)
 geom <- function (xs) exp(sum(log(xs[xs>0]),na.rm=T)/length(xs))
-harmonic <- function (xs) length(xs)/sum(1/xs[xs>0],na.rm=T)
 set.seed(334)
-highwayLength <- 5000
-maxCars <- 200
-lanes <- 3
-lastLane <- lanes
-gencars <- function (maxCars,lane) {
-    x <- sort(unique(trunc(runif(maxCars, 1, highwayLength))))
-    aggressive <- x%%20==0
-    data.frame(x,lane,speed=0,aggressive)
+levels <- 1:3
+c(car,moto,truck) %<-% factor(levels)
+len <- function (vehicles) ifelse(vehicles == moto, 2.2, ifelse(vehicles == car, 4.5, 14.6))
+mass <- function (vehicles) ifelse(vehicles == moto, 175, ifelse(vehicles == car, 1850, 9750))
+accel <- function (vehicles) ifelse(vehicles == moto, 7, ifelse(vehicles == car, 3, 0.6))
+vmax <- function (vehicles) ifelse(vehicles == truck, 29, 32.5)
+vehicles.random <- function (n) {
+    vehicles <- rbinom(n,1,0.73)
+    factor(ifelse(vehicles, vehicles, rbinom(n,1,0.25/0.27)+2),levels)
 }
-highway <- lapply(letters[1:lanes], function (lane) gencars(maxCars/ifelse(lane==1||lane==lastLane,2,1),lane))
+hw <- list(lanes = 3, vehicles = 200, length = 5000)
+lane.random <- function (lane) {
+    n <- hw$vehicles/ifelse(lane==1||lane==hw$lanes,2,1)
+    position <- sort(unique(trunc(runif(hw$length, 1, hw$length)))[1:n])
+    car.type <- vehicles.random(n)
+    speed <- (rbeta(n,2,0.5)/2+0.5)*vmax(car.type)
+    data.frame(position,car.type,lane,speed)
+}
+highway <- lapply(1:hw$lane, lane.random)
+# lane1 <- highway[[1]]
+# lane1$position <- (lane1$position + vmax(lane1$car.type)) %% hw$length
+# lane1
+
+# duration <- 3600
+# stepby <- 36
+
+# step <- function (time, rules, highways, highway) {
+#     if (time > duration)
+#         rbind(highways, highway)
+#     else if (time %% stepby == 0)
+#         step(time+1,rbind(highways,highway),highway)
+#     else
+# }
+
+highways <- data.frame()
+i <- 0
+repeat {
+    if (i >= 100)
+        break;
+    for (lane in highway) {
+        lane$position <- (lane$position + i*lane$speed) %% 5000
+        lane$lane <- lane$lane + i/100
+        highways <- rbind(highways,lane)
+    }
+    i <- i+1
+}
 pdf("test.pdf",width=12)
-ggplot(mapping=aes(x=x,y=lane,col=aggressive)) +
-    geom_point(data=highway[[1]]) +
-    geom_point(data=highway[[2]]) +
-    geom_point(data=highway[[3]]) +
-    scale_color_manual(values=c("black","red"))
+map <- aes_(
+    x=~position,
+    y=~lane,
+    size=quote(car.type)
+)
+ggplot() +
+    scale_size_manual(values=c(0.1,0.5,2),labels=c("Moto","Car","Truck")) +
+    geom_point(map, highways, show.legend=F)
 dev.off()
-system("explorer test.pdf")
+system("xdg-open test.pdf")
