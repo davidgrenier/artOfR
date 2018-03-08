d2a1232b87e0091dc9896c65c0624b21d11ba609
David Grenier <davidgrenier@gmail.com>
2018-03-08 12:16:32 -0500

    Files

--- projet1.r
+++ projet1.r
@@ -2,62 +2,72 @@
 .libPaths('.')
 library(ggplot2)
 library(zeallot)
+library(data.table)
 geom <- function (xs) exp(sum(log(xs[xs>0]),na.rm=T)/length(xs))
 set.seed(334)
-levels <- 1:3
-c(car,moto,truck) %<-% factor(levels)
-len <- function (vehicles) ifelse(vehicles == moto, 2.2, ifelse(vehicles == car, 4.5, 14.6))
-mass <- function (vehicles) ifelse(vehicles == moto, 175, ifelse(vehicles == car, 1850, 9750))
-accel <- function (vehicles) ifelse(vehicles == moto, 7, ifelse(vehicles == car, 3, 0.6))
-vmax <- function (vehicles) ifelse(vehicles == truck, 29, 32.5)
-vehicles.random <- function (n) {
+levels <- factor(1:3)
+c(car,moto,truck) %<-% levels
+v.length <- function (vs) ifelse(vs == moto, 2.2, ifelse(vs == car, 4.5, 14.6))
+v.mass <- function (vs) ifelse(vs == moto, 175, ifelse(vs == car, 1850, 9750))
+v.accel <- function (vs) ifelse(vs == moto, 7, ifelse(vs == car, 3, 0.6))
+v.vmax <- function (vs) ifelse(vs == truck, 29, 32.5)
+v.random <- function (n) {
     vehicles <- rbinom(n,1,0.73)
     factor(ifelse(vehicles, vehicles, rbinom(n,1,0.25/0.27)+2),levels)
 }
-hw <- list(lanes = 3, vehicles = 200, length = 5000)
+hw <- list(lanes = 3, vehicles = 200, length = 120000)
 lane.random <- function (lane) {
     n <- hw$vehicles/ifelse(lane==1||lane==hw$lanes,2,1)
     position <- sort(unique(trunc(runif(hw$length, 1, hw$length)))[1:n])
-    car.type <- vehicles.random(n)
-    speed <- (rbeta(n,2,0.5)/2+0.5)*vmax(car.type)
-    data.frame(position,car.type,lane,speed)
+    v.type <- v.random(n)
+    speed <- (rbeta(n,2,0.5)/2+0.5)*v.vmax(v.type)
+    data.frame(position,v.type,lane,speed)
 }
-highway <- lapply(1:hw$lane, lane.random)
-# lane1 <- highway[[1]]
-# lane1$position <- (lane1$position + vmax(lane1$car.type)) %% hw$length
-# lane1
+highway <- lapply(seq(hw$lane), lane.random)
 
-# duration <- 3600
-# stepby <- 36
-
-# step <- function (time, rules, highways, highway) {
-#     if (time > duration)
-#         rbind(highways, highway)
-#     else if (time %% stepby == 0)
-#         step(time+1,rbind(highways,highway),highway)
-#     else
-# }
-
-highways <- data.frame()
-i <- 0
-repeat {
-    if (i >= 100)
-        break;
-    for (lane in highway) {
-        lane$position <- (lane$position + i*lane$speed) %% 5000
-        lane$lane <- lane$lane + i/100
-        highways <- rbind(highways,lane)
+stepby <- 36
+duration <- 3600
+run <- function (highway, rules) {
+    highways <- list()
+    i <- 1
+    time <- 0
+    repeat {
+        if (time %% stepby == 0) {
+            highways[[i]] <- rbindlist(highway)
+            i <- i+1
+            if (time >= duration)
+                break;
+        }
+        highway <- rules(highway)
+        time <- time+1
     }
-    i <- i+1
+    data.frame(rbindlist(highways))
 }
+
+rules.basic <- function (highway) {
+    safety <- 5
+    cartoofar <- list(position=9999999,v.type=car,lane=1,speed=0)
+    for (i in seq(highway)) {
+        lane <- highway[[i]]
+        t <- tail(lane, -1)
+        t <- rbind(t,cartoofar)
+        saferoom <- t$position + t$speed - (lane$position + v.length(lane$v.type) + safety + lane$speed)
+        accel <- pmin(saferoom, lane$position + v.length(lane$v.type), v.vmax(lane$v.type)-lane$speed, v.accel(lane$v.type))
+        lane$speed <- lane$speed + accel
+        lane$position <- (lane$position + lane$speed) %% hw$length
+        lane$lane <- lane$lane + 1/(stepby*100)
+        highway[[i]] <- lane
+    }
+    highway
+}
+
+highways <- run(highway, rules.basic)
+
 pdf("test.pdf",width=12)
-map <- aes_(
-    x=~position,
-    y=~lane,
-    size=quote(car.type)
-)
+map <- aes(x=position, y=lane, size=v.type, color=v.type)
 ggplot() +
-    scale_size_manual(values=c(0.1,0.5,2),labels=c("Moto","Car","Truck")) +
+    scale_size_manual(values=c(0.1,0.1,0.8),labels=c("Car","Moto","Truck")) +
+    scale_color_manual(values=c("Black", "Red","Black")) +
     geom_point(map, highways, show.legend=F)
 dev.off()
 system("xdg-open test.pdf")
